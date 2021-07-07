5ca20430963121145d601ed1be41cbb39e80cf90
Pratyaksh Tyagi <55044774+pratyaksh123@users.noreply.github.com>
2021-07-07 23:37:52 +0530

    FetchProblem from DOM implemented

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,27 +1,46 @@
 import isProblem from '../../utils/isProblem'
-import FetchProblemData from "../../utils/fetchProblemData"
+import FetchProblemData from '../../utils/fetchProblemData'
 
 const URL = window.location.toString()
 const check = isProblem(URL)
+// let problem = null
 
-if (check) {
-    window.addEventListener("load", myMain, false);
+chrome.runtime.onMessage.addListener(
+    function (message, sender, sendResponse) {
+        const res = fetchProblemFromDOM().then((t) => {
+            console.log(t.data)
+            sendResponse(t);
+        })
 
-    function myMain(evt) {
-        var jsInitChecktimer = setInterval(checkForJS_Finish, 111);
+        return true;
+    });
+
+async function fetchProblemFromDOM() {
+    return new Promise((resolve, reject) => {
+        console.log('Starting to Execute content script')
+        console.log(`Contenet script ${Math.random()}`)
+        var jsInitChecktimer = setInterval(checkForJS_Finish, 111)
 
         function checkForJS_Finish() {
-            if (document.getElementsByClassName('css-v3d350') !== undefined && document.getElementById("timer-difficulty") !== null) {
-                clearInterval(jsInitChecktimer);
-                const problemData = document.getElementsByClassName('css-v3d350')[0].textContent
-                const problemDifficulty = document.getElementById("timer-difficulty").firstElementChild.textContent
+            if (
+                document.getElementsByClassName('css-v3d350') !== undefined &&
+                document.getElementById('timer-difficulty') !== null
+            ) {
+                clearInterval(jsInitChecktimer)
+                const problemData = document.getElementsByClassName('css-v3d350')[0]
+                    .textContent
+                const problemDifficulty = document.getElementById('timer-difficulty')
+                    .firstElementChild.textContent
                 const problem = FetchProblemData(problemData, problemDifficulty)
-                chrome.storage.sync.set({ currentProblem: problem }, function () {
-                    console.log('Value is set to ' + value);
-                });
+                console.log(problem)
+                resolve({ data: problem })
             }
         }
-    }
+    })
+
 }
 
-
+if (check) {
+    window.addEventListener('load', fetchProblemFromDOM, false)
+    fetchProblemFromDOM()
+}
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import NoProblemPage from '../../Components/NoProblemsPage/NoProblemPage'
+import FetchProblemData from '../../utils/fetchProblemData'
 import isProblem from '../../utils/isProblem'
 
 const Popup = () => {
   const [url, setUrl] = useState('')
+  const [currentProblem, setCurrentProblem] = useState(null)
 
   useEffect(() => {
-    console.log("HI")
     chrome.tabs.query(
       {
         active: true,
@@ -15,14 +16,21 @@ const Popup = () => {
       (tabs) => {
         let tab = tabs[0]
         setUrl(tab.url)
+        chrome.tabs.sendMessage(tab.id, { action: 'executeCode' },(response)=>{
+          console.log(response)
+          setCurrentProblem(response.data)
+        });
       }
     )
   }, [])
 
-  if (url !== '' && isProblem(url)) {
+  if (url !== '' && isProblem(url)){
     return (
       <div className="App">
         <p>Problem</p>
+        {currentProblem && (<p>{currentProblem.problemName}</p>)}
+        {currentProblem && (<p>{currentProblem.problemId}</p>)}
+        {currentProblem && (<p>{currentProblem.problemDifficulty}</p>)}
       </div>
     )
   } else {
